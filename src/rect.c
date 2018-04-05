d002d4910b6115d3c100ba4aa2c21d2ffdd077da
tracer <tracer@tracer.org>
2018-04-05 09:48:52 -0700

    > run mabai Linux ubuntu 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/rect.c
+++ src/rect.c
@@ -67,7 +67,7 @@ void rect_draw(Rect* rect){
     int f_color = color_to_int(rect->property.f_color);
     unsigned color_buf[WIDTH * HEIGHT];
     memset(color_buf, 0 ,sizeof(int) * WIDTH * HEIGHT);
-    printf("%d %d\n", rect->property.height, rect->property.width);
+    //printf("%d %d\n", rect->property.height, rect->property.width);
     for(int i = 0; i < rect->property.height; i++)
     {
     	for(int j = 0; j < rect->property.width; j++)

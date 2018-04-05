49604c891285f201526f2bb0b18812bb88ed5e73
tracer <tracer@tracer.org>
2018-04-05 09:42:24 -0700

    > run mabai Linux ubuntu 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/rect.c
+++ src/rect.c
@@ -95,6 +95,9 @@ void rect_draw(Rect* rect){
     reg.w = rect->property.width;
     reg.h = rect->property.height;
     reg.sync = 0;
+    
+    printf("%d %d %d %d", reg.x, reg.y, reg.w, reg.h)
+    
     _Device *dev = getdev(&video_dev, _DEV_VIDEO);
     dev->write(_DEVREG_VIDEO_FBCTL, &reg, sizeof(_FBCtlReg));
     draw_sync();

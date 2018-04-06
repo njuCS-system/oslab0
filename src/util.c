2c1dd427be25b67798c603f7bb87b8d654184af3
tracer <tracer@tracer.org>
2018-04-05 20:31:31 -0700

    > run mabai Linux ubuntu 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/util.c
+++ src/util.c
@@ -5,3 +5,28 @@ unsigned color_to_int(Color color)
     unsigned *return_val = (unsigned *)&color;
     return *return_val;
 }
+
+
+_Device *getdev(_Device **ptr, uint32_t id) {
+  if (*ptr) return *ptr;
+  for (int n = 1; ; n ++) {
+    _Device *cur = _device(n);
+    if (cur->id == id) {
+      *ptr = cur;
+      return cur;
+    }
+    if (!cur) break;
+  }
+  //assert(0);
+  return NULL;
+}
+
+
+void draw_sync() {
+  _Device *dev = getdev(&video_dev, _DEV_VIDEO);
+  _FBCtlReg ctl;
+  ctl.pixels = NULL;
+  ctl.x = ctl.y = ctl.w = ctl.h = 0;
+  ctl.sync = 1;
+  dev->write(_DEVREG_VIDEO_FBCTL, &ctl, sizeof(ctl));
+}

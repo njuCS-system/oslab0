d9e13a5c76ae1a49bae520341f773c40e648a05f
tracer <tracer@tracer.org>
2018-04-06 03:10:45 -0700

    > build mabai Linux ubuntu 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -226,10 +226,12 @@ void try(){
   if(bullet != NULL)
   {
     cp_virtual_draw(bullet);
-    for(int i =0; i < 10; i++)
+    /*for(int i =0; i < 10; i++)
     {
       mv_virtual_automove(bullet);
       cp_virtual_draw(bullet);
-    }
+    }*/
+    cp_virtual_move(planeI, 10, 20);
+    cp_virtual_draw(planeI);
   }
 }

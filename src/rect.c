550b87ddb5674a39dca4571ac6d9919b61e53afb
tracer <tracer@tracer.org>
2018-04-05 18:40:10 -0700

    > run mabai Linux ubuntu 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/rect.c
+++ src/rect.c
@@ -93,7 +93,7 @@ void rect_draw(Rect* rect){
     }
     ctl.x = rect->property.x;
     ctl.y = rect->property.y;
-    ctl.pixels = color_buf;
+    //ctl.pixels = color_buf;
     ctl.w = rect->property.width;
     ctl.h = rect->property.height;
     ctl.sync = 0;

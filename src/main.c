5ab3eef31fd96c3e6cd8ca900775d5daee44793e
tracer <tracer@tracer.org>
2018-04-05 19:43:33 -0700

    > run mabai Linux ubuntu 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -139,7 +139,7 @@ static void ata_test(_Device *dev) {
 
 void try(){
   RectProperty p;
-  /*unsigned a[100000];
+  unsigned a[100000];
   memset(a, 0 , 40000);
   for(int i =0;i<100000;i++)
   {
@@ -148,10 +148,7 @@ void try(){
   if(a != 0)
   {
   
-  }*/
-  
-  unsigned a[100000];
-  printf("hello\n");
+  }
   
   p.s_color.r = 255;
   p.s_color.g = 255;

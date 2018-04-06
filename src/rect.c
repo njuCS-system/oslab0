876ccfb63a47c7f13691959e9cc79056ad14bada
tracer <tracer@tracer.org>
2018-04-05 18:40:23 -0700

    > run mabai Linux ubuntu 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/rect.c
+++ src/rect.c
@@ -63,8 +63,8 @@ void draw_sync() {
 
 void rect_draw(Rect* rect){
     _FBCtlReg ctl;
-    unsigned s_color = color_to_int(rect->property.s_color);
-    unsigned f_color = color_to_int(rect->property.f_color);
+    //unsigned s_color = color_to_int(rect->property.s_color);
+    //unsigned f_color = color_to_int(rect->property.f_color);
     //unsigned color_buf[WIDTH * HEIGHT];
     //memset(color_buf, 0 ,sizeof(int) * WIDTH * HEIGHT);
     //printf("hello1\n");

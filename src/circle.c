f1346a073f226c4904ca84c222238c121ca7483b
tracer <tracer@tracer.org>
2018-04-06 19:46:50 -0700

    > run mabai Linux ubuntu 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/circle.c
+++ src/circle.c
@@ -65,9 +65,9 @@ void circle_draw(Circle* circle){
     
     ctl.pixels = color_buf;
     
-    _Device *dev = getdev(&video_dev, _DEV_VIDEO);
+    /*_Device *dev = getdev(&video_dev, _DEV_VIDEO);
     dev->write(_DEVREG_VIDEO_FBCTL, &ctl, sizeof(_FBCtlReg));
-    draw_sync();
+    draw_sync();*/
     fb_add(&ctl);
 }
 

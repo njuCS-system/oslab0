5d928858e9317a39068742d43dd92fd9f437a92a
tracer <tracer@tracer.org>
2018-04-08 09:29:10 -0700

    > build mabai Linux ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- src/util.c
+++ src/util.c
@@ -6,6 +6,33 @@ unsigned color_to_int(Color color)
     return *return_val;
 }
 
+RES_IDC res_idc[] =
+	{
+		{ID_PLAYER, {50}, {0, 0, 29, 33}},
+		{ID_ENEMY_SMALL, {20}, {41, 0, 33, 28}},
+		{ID_ENEMY_MEDIUM, {50}, {74, 0, 43, 42}},
+		{ID_ENEMY_BIG, {100}, {0, 33, 62, 50}},
+		{ID_ROUNDBULLET_0, {6}, {29, 13, 7, 7}},
+		{ID_ROUNDBULLET_1, {12}, {36, 0, 5, 5}},
+		{ID_DOWNMISSILE, {13}, {29, 0, 3, 13}},
+		{ID_UPMISSILE, {13}, {32, 0, 4, 8}},
+		{ID_SMALLBOMB_0, {0}, {32 * 0, 83, 32, 32}},
+		{ID_SMALLBOMB_1, {0}, {32 * 1, 83, 32, 32}},
+		{ID_SMALLBOMB_2, {0}, {32 * 2, 83, 32, 32}},
+		{ID_SMALLBOMB_3, {0}, {32 * 3, 83, 32, 32}},
+		{ID_SMALLBOMB_4, {0}, {32 * 4, 83, 32, 32}},
+		{ID_SMALLBOMB_5, {0}, {32 * 5, 83, 32, 32}},
+		{ID_SMALLBOMB_6, {0}, {32 * 6, 83, 32, 32}},
+		{ID_SMALLBOMB_7, {0}, {32 * 7, 83, 32, 32}},
+		{ID_BIGBOMB_0, {0}, {180 * 0, 115 + 135 * 0, 180, 135}},
+		{ID_BIGBOMB_1, {0}, {180 * 1, 115 + 135 * 0, 180, 135}},
+		{ID_BIGBOMB_2, {0}, {180 * 2, 115 + 135 * 0, 180, 135}},
+		{ID_BIGBOMB_3, {0}, {180 * 3, 115 + 135 * 0, 180, 135}},
+		{ID_BIGBOMB_4, {0}, {180 * 0, 115 + 135 * 1, 180, 135}},
+		{ID_BIGBOMB_5, {0}, {180 * 1, 115 + 135 * 1, 180, 135}},
+		{ID_BIGBOMB_6, {0}, {180 * 2, 115 + 135 * 1, 180, 135}},
+		{ID_BIGBOMB_7, {0}, {180 * 3, 115 + 135 * 1, 180, 135}}
+	}
 
 _Device *getdev(_Device **ptr, uint32_t id) {
   if (*ptr) return *ptr;

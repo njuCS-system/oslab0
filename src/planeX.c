0c5ebe08e321d0b5cfc67802710eba04afeb6c85
tracer <tracer@tracer.org>
2018-04-07 19:39:37 -0700

    > run mabai Linux ubuntu 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/planeX.c
+++ src/planeX.c
@@ -44,15 +44,15 @@ void planeX_draw(PlaneX* planeX){
     int size = planeX->property.size;
     
     //                x    y     w   h     s_color        f_color  stroke is_fill
-    RectProperty p0 = {x, y + 16 * size, 30 * size, 6 * size, {0, 0, 0, 0}, {255, 0, 0, 0}, 1, TRUE};
+    RectProperty p0 = {x, y + 16 * size, 30 * size, 6 * size, {0, 0, 0, 0}, {49, 125, 237, 0}, 1, TRUE};
     planeX->large_wing = build_rect(p0);
-    RectProperty p1 = {x + 8 * size, y + 2 * size, 14 * size, 3 * size, {0, 0, 0, 0}, {255, 0, 0, 0}, 1, TRUE};
+    RectProperty p1 = {x + 8 * size, y + 2 * size, 14 * size, 3 * size, {0, 0, 0, 0}, {49, 125, 237, 0}, 1, TRUE};
     planeX->small_wing = build_rect(p1);
     RectProperty p2 = {x + 9 * size, y, 3 * size, 6 * size, {0, 0, 0, 0}, {240, 176, 0, 0}, 1, TRUE};
     planeX->empennage_left = build_rect(p2);
     RectProperty p3 = {x + 18 * size, y, 3 * size, 6 * size, {0, 0, 0, 0}, {240, 176, 0, 0}, 1, TRUE};
     planeX->empennage_right = build_rect(p3);
-    RectProperty p4 = {x + 13 * size, y + 2 * size, 4 * size, 14 * size, {0, 0, 0, 0}, {49, 125, 237, 0}, 1, TRUE};
+    RectProperty p4 = {x + 13 * size, y + 2 * size, 4 * size, 14 * size, {0, 0, 0, 0}, {71, 173, 112, 0}, 1, TRUE};
     planeX->body = build_rect(p4);
     RectProperty p5 = {x + 5 * size, y + 15 * size, 3 * size, 9 * size, {0, 0, 0, 0}, {240, 176, 0, 0}, 1, TRUE};
     planeX->jet_left = build_rect(p5);

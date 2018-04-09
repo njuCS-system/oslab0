4640fb14ceffcc39d2bf01b2fd2f21a7ddadf80a
tracer <tracer@tracer.org>
2018-04-09 08:14:13 -0700

    > run mabai Linux ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- src/big_explosion.c
+++ src/big_explosion.c
@@ -54,9 +54,9 @@ void bigExplosion_draw(BigExplosion* bigExplosion){
         for(int j = 0; j < width; j++)
         {
             Color temp_color;
-            temp_color.b = bmpHead -> pixels[((i + x) * width + (j + y) * 3];
-            temp_color.g = bmpHead -> pixels[((i + x) * width + (j + y) * 3 + 1];
-            temp_color.r = bmpHead -> pixels[((i + x) * width + (j + y) * 3 + 2];
+            temp_color.b = bmpHead -> pixels[(i + x) * width + (j + y) * 3];
+            temp_color.g = bmpHead -> pixels[(i + x) * width + (j + y) * 3 + 1];
+            temp_color.r = bmpHead -> pixels[(i + x) * width + (j + y) * 3 + 2];
             if(temp_color.b == 255 && temp_color.g == 0 && temp_color.r == 255)
             {
                 temp_color.a = 1;

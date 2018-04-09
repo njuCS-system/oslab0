0c9f1307aa18001183f7c35e36e2ab657ff636c3
tracer <tracer@tracer.org>
2018-04-09 05:35:33 -0700

    > .DEFAULT mabai Linux ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- src/planeX.c
+++ src/planeX.c
@@ -39,34 +39,44 @@ void planeX_delete(PlaneX* planeX){
 
 void planeX_draw(PlaneX* planeX){
     _FBCtlReg ctl;
-    int x = planeX->property.x;
-    int y = planeX->property.y;
-    int size = planeX->property.size;
+
+    BITMAPINFOHEADER *bmpHead = (BITMAPINFOHEADER*) (model_planex + BITMAPFILEHEADER_LENGTH);
+    printf("%d\n", sizeof(BITMAPFILEHEADER));
+    long width = bmpHead -> biWidth;
+    long height = bmpHead -> biHeight;
+    printf("%d %d\n", width, height);
+
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            Color temp_color;
+            temp_color.b = bmpHead -> pixels[(i * width + j) * 3];
+            temp_color.g = bmpHead -> pixels[(i * width + j) * 3 + 1];
+            temp_color.r = bmpHead -> pixels[(i * width + j) * 3 + 2];
+            if(temp_color.b == 1 && temp_color.g == 1 && temp_color.r == 1)
+            {
+                temp_color.a = 1;
+            }
+            else
+            {
+                temp_color.a = 0;
+            }
+            color_buf[i * width + j] = color_to_int(temp_color);
+        }
+    }
     
-    //                x    y     w   h     s_color        f_color  stroke is_fill
-    RectProperty p0 = {x, y + 16 * size, 30 * size, 6 * size, {0, 0, 0, 0}, {49, 125, 237, 0}, 1, TRUE};
-    planeX->large_wing = build_rect(p0);
-    RectProperty p1 = {x + 8 * size, y + 2 * size, 14 * size, 3 * size, {0, 0, 0, 0}, {49, 125, 237, 0}, 1, TRUE};
-    planeX->small_wing = build_rect(p1);
-    RectProperty p2 = {x + 9 * size, y, 3 * size, 6 * size, {0, 0, 0, 0}, {240, 176, 0, 0}, 1, TRUE};
-    planeX->empennage_left = build_rect(p2);
-    RectProperty p3 = {x + 18 * size, y, 3 * size, 6 * size, {0, 0, 0, 0}, {240, 176, 0, 0}, 1, TRUE};
-    planeX->empennage_right = build_rect(p3);
-    RectProperty p4 = {x + 13 * size, y + 2 * size, 4 * size, 14 * size, {0, 0, 0, 0}, {71, 173, 112, 0}, 1, TRUE};
-    planeX->body = build_rect(p4);
-    RectProperty p5 = {x + 5 * size, y + 15 * size, 3 * size, 9 * size, {0, 0, 0, 0}, {240, 176, 0, 0}, 1, TRUE};
-    planeX->jet_left = build_rect(p5);
-    RectProperty p6 = {x + 22 * size, y + 15 * size, 3 * size, 9 * size, {0, 0, 0, 0}, {240, 176, 0, 0}, 1, TRUE};
-    planeX->jet_right = build_rect(p6);
-    
-    rect_draw(planeX->large_wing);
-    rect_draw(planeX->small_wing);
-    rect_draw(planeX->empennage_left);
-    rect_draw(planeX->empennage_right);
-    rect_draw(planeX->body);
-    rect_draw(planeX->jet_left);
-    rect_draw(planeX->jet_right);
-    
+    ctl.x = planex->property.x;
+    ctl.y = planex->property.y;
+    ctl.pixels = color_buf;
+    ctl.w = width;
+    ctl.h = height;
+    ctl.sync = 0;
+
+    _Device *dev = getdev(&video_dev, _DEV_VIDEO);
+    dev->write(_DEVREG_VIDEO_FBCTL, &ctl, sizeof(_FBCtlReg));
+    draw_sync();
+
     fb_add(&ctl);
 }
 

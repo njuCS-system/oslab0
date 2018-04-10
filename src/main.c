030cc9890628e313b99256fae6affe0c2dd9e04a
tracer <tracer@tracer.org>
2018-04-10 05:39:04 -0700

    > run mabai Linux ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -395,16 +395,16 @@ void try(){
   //try_planeX();
   //try_planeU();
   //try_planeT();
-  //try_player1();
+  try_player1();
   //try_bullet();
   //try_bigExplosion3();
   //try_hp();
   
-  /*while(1)
+  while(1)
   {
     kbRespond_action();
     screen_draw();
-  }*/
+  }
   
   //screen_draw();
 

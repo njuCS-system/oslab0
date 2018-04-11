b30b1a251d7c215b73760db73685d91134accad6
tracer <tracer@tracer.org>
2018-04-11 00:19:56 -0700

    > run mabai Linux ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- src/big_explosion3.c
+++ src/big_explosion3.c
@@ -41,7 +41,7 @@ void bigExplosion3_draw(BigExplosion3* bigExplosion3){
     _FBCtlReg ctl;
 
     BITMAPINFOHEADER *bmpHead = (BITMAPINFOHEADER*) (model_objset + BITMAPFILEHEADER_LENGTH);
-    RECT temp_rect = res_idc[ID_BIGBOMB_3].rect;
+    UTIL_RECT temp_rect = res_idc[ID_BIGBOMB_3].rect;
     long pic_width = bmpHead -> biWidth;
     //long pic_height = bmpHead -> biHeight;
     unsigned width = temp_rect.w;

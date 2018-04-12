8ad7e1f1502c9d19b6d291702ae858f0d3210079
tracer <tracer@tracer.org>
2018-04-12 06:21:22 -0700

    > run mabai Linux ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- src/game.c
+++ src/game.c
@@ -63,7 +63,7 @@ void main_loop()
     game_init();
 
     const int delay = 0;
-    const int shooting_loop_count = 15;
+    const int shooting_loop_count = 30;
 
     unsigned now_time = uptime();
     unsigned last_time = 0;
@@ -155,7 +155,7 @@ static void boundary_detect()
 static void random_create_plane()
 {
     const int random_range = 10000;
-    const int plane_occur = random_range * 0.03;
+    const int plane_occur = random_range * 0.01;
     const int planeI_occur = plane_occur * 0.4;
     const int planeX_occur = plane_occur * 0.3 + planeI_occur;
     const int planeT_occur = plane_occur * 0.2 + planeX_occur;

6040cf57e131aae7d6f69f2f431de39f290f37bc
tracer <tracer@tracer.org>
2018-04-12 04:28:03 -0700

    > run mabai Linux ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- src/game.c
+++ src/game.c
@@ -53,7 +53,7 @@ void main_loop()
     game_init();
 
     const int delay = 30;
-    const int shooting_loop_count = 10;
+    const int shooting_loop_count = 100;
 
     unsigned now_time = uptime();
     unsigned last_time = 0;

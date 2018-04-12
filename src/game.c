3dc91214ad378cf927768e9fa1fa68e2a304be4f
tracer <tracer@tracer.org>
2018-04-12 02:20:41 -0700

    > run mabai Linux ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- src/game.c
+++ src/game.c
@@ -2,9 +2,12 @@
 
 Game game;
 static int init_x = 0;
-static int init_y = 0;
-//                            x       y   vx  vy size h_max
-static PlaneIProperty i = {init_x, init_y, 0, 10, 1, 100};
+
+//                         x  y vx  vy size h_max
+static PlaneIProperty i = {0, 0, 0, 6, 1, 100};
+static PlaneXProperty x = {0, 0, 0, 6, 1, 200};
+static PlaneTProperty t = {0, 0, 0, 10, 1, 200};
+static PlaneUProperty u = {0, 0, 0, 15, 1, 400};
 
 static void __add(Game* s,void* object);
 
@@ -13,6 +16,8 @@ static void __draw(Game* s);
 static void __clear(Game* s);
 
 static void __remove(Game* s,void* object);
+
+static void __move(Game* s);
 
 static void game_init();
 
@@ -24,16 +29,34 @@ static void game_draw();
 
 static void game_rm(void * obj);
 
+static void game_move();
+
+void random_create_plane();
 
 
-void random_create_plane()
+void random_init_x()
 {
-
+    init_x = rand() % (_WIDTH - PLANE_WIDTH);
 }
 
 void main_loop()
 {
-    srand(uptime());
+    game_init();
+    unsigned now_time = uptime();
+    unsigned last_time = 0;
+    srand(now_time);
+    while(1)
+    {
+        if(now_time >= last_time + 200)
+        {
+            random_create_plane();
+            game_move();
+            game_draw();
+            last_time = now_time;
+        }
+        now_time = uptime();
+    }
+    
 }
 
 static void game_init()
@@ -63,6 +86,49 @@ static void game_rm(void * obj){
     __remove(&game, obj);
 }
 
+static void game_move()
+{
+    __move(&game);
+}
+
+void random_create_plane()
+{
+    const int random_range = 1000;
+    const int plane_occur = random_range * 0.1;
+    const int planeI_occur = plane_occur * 0.4;
+    const int planeX_occur = plane_occur * 0.3 + planeI_occur;
+    const int planeT_occur = plane_occur * 0.2 + planeX_occur;
+    const int planeU_occur = plane_occur * 0.1 + planeU_occur;
+    random_init_x;
+    int r = rand() % random_range;
+    if(r < plane_occur)
+    {
+        if(r < planeI_occur)
+        {
+            i.x = init_x;
+            PlaneI *planeI = build_planeT(i);
+            game_add(planeI);
+        }
+        else if(r < planeX_occur)
+        {
+            x.x = init_x;
+            PlaneX *planeX = build_planeT(x);
+            game_add(planeX);
+        }
+        else if(r < planeT_occur)
+        {
+            t.x = init_x;
+            PlaneT *planeT = build_planeT(t);
+            game_add(planeT);
+        }
+        else if(r < planeU_occur)
+        {
+            u.x = init_x;
+            PlaneU *planeU = build_planeT(u);
+            game_add(planeU);
+        }
+    }
+}
 
 //****************************************************************
 static void __add(Game* s,void* object){
@@ -99,4 +165,14 @@ static void __clear(Game* s){
 static void __remove(Game* s, void *obj)
 {
     screen_rm(obj);
+}
+
+
+static void __move(Game* s)
+{
+    for(int i = 0;i < OBJ_MAX;i++){
+        if(((Info *)(s->obj[i]))->valid == TRUE){
+            mv_virtual_automove(s->obj[i]);
+        }
+    }
 }

fc1e730e6fa04d690c090d27d3526071d53d5fcf
tracer <tracer@tracer.org>
2018-04-16 00:56:05 -0700

    > .DEFAULT mabai Linux ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- src/game.c
+++ src/game.c
@@ -370,7 +370,7 @@ static void __create_enemy_bullet(Game *s)
     const int bullet_speed = 18;
     const int bullet_offset = 10;
     const int random_range = 1000;
-    const int attack = random_range * 0.3;
+    const int attack = random_range * 1;
     for(int i = 0;i < OBJ_MAX;i++){
         if(((Info *)(s->obj[i]))->valid == TRUE){
             UTIL_RECT ur;
